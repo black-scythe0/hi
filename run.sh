9f7d35472376d434c09fcba9dcd63c4fe4841316
Klaus10101 <dhamakacraftgaming@gmail.com>
2025-04-20 15:46:07 +0000

    bash imp

--- run.sh
+++ run.sh
@@ -1,3 +1,23 @@
 #/data/data/com.termux/files/usr/bin/bash
 
-python hi.py
+
+
+pythonId=$(pgrep python3)
+
+
+if [ -n "${pythonId}" ]; then
+    echo "killing running py process.."
+    kill -9 "$pythonId"
+    killed=true
+
+else
+    echo "no py process running."
+    echo "starting server"
+
+    python3 hi.py
+    
+fi
+
+
+echo "$pythonid"
+
