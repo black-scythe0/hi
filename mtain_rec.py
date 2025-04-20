dc20dce27560c90bd0596ae0131e0c38d4299517
Klaus10101 <dhamakacraftgaming@gmail.com>
2025-04-20 07:34:37 +0000

    rec_handle[1/3 done]

--- mtain_rec.py
+++ mtain_rec.py
@@ -1,16 +1,35 @@
 #! usr/bin/python3
 
 ### Part:-
-### to maintain data in text files ###
+### to maintain data in single files ###
 
 
+import os
 
-
+import pickle  # just using it for now, sure to impliment a better one in future
 
 class mtain_rec:
-    def __init__(self):
-        pass
 
-    def write():
+    def __init__(self, user_data,file):
+        self.user_data = user_data
+        self.file = file
+
+    def check_if_exist(self):
         pass
+        
+
+    def write_rec(self):
+        try:
+             with open(f"{self.file}", "ab") as rec_file:
+                pickle.dump(self.user_data,rec_file)
+        except Exception as e:
+                assert e != None, "ERRoR OpeNNiNg FiLe!"
+                
+    def read_rec(self,file):
+        
+        with open(f"{self.file}", "rb") as rec_file:
+            rec_data = pickle.load(rec_file)
+            return rec_data
     
+        
+
