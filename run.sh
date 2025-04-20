f1f7f4d285b4c4e1d383dc2fbd3aef71ae771b1f
Klaus10101 <dhamakacraftgaming@gmail.com>
2025-04-20 16:31:35 +0000

    changes in run

--- run.sh
+++ run.sh
@@ -13,9 +13,13 @@ if [ -n "${pythonId}" ]; then
 else
     echo "no py process running."
     echo "starting server"
-
-    python3 hi.py
+    echo " see error.txt for more info "
+    date>> error.txt
+    echo " ">> error.txt
     
+    python3 hi.py --verbose 2>> error.txt
+    echo " ">> error.txt
+    echo " ">> error.txt
 fi
 
 
